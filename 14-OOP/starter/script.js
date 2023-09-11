23e54f31c2ea7c52011b17524b000a20202e36e6
kaushik.bora <herock76@gmail.com>
2023-09-11 21:33:59 +0530

    -getters and setters complete

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -66,37 +66,89 @@ console.log(Object.getPrototypeOf(car1)); */
 
 //class declaration
 
-class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
-    this.birthYear = birthYear;
-  }
-
-  calcAge() {
-    console.log(new Date().getFullYear() - this.birthYear);
-  }
-
-  greet = function () {
-    console.log('Hello ' + this.firstName);
-  };
-}
-
-/**
- * calcAge and greet not on the PersonCl but on the prototype property, i.e, PersonCl.prototype
- */
-
-const jessica = new PersonCl('Jessica', 1998);
-
-jessica.calcAge();
-
-// PersonCl.prototype.greet = function () {
-//   console.log('Hello ' + this.firstName);
-// };
-
-jessica.greet();
-
 /**
  *  1. Classes are not hoisted unlike function constructors
  *  2. Classes are first-class citizens
  *  3. Classes are executed in strict mode
  */
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  /**
+   * {@link calcAge} - returns age of person
+   */
+  calcAge() {
+    return new Date().getFullYear() - this.birthYear;
+  }
+
+  greet = function () {
+    console.log('Hello ' + this.fullName);
+  };
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  set fullName(name) {
+    if (name.includes(' ')) {
+      this._fullName = name;
+    } else {
+      alert(name + ' is not a valid full name!');
+    }
+  }
+}
+
+/**
+ * @calcAge and @greet are not on the PersonCl object but on the prototype property, i.e, PersonCl.prototype
+ */
+
+const jessica = new PersonCl('Jessica Davis', 1998);
+
+// jessica.calcAge();
+
+// console.log(jessica.fullName);
+// console.log(jessica);
+
+// jessica.fullName = 'Jessica Davis';
+
+// PersonCl.prototype.greet = function () {
+//   console.log('Hello ' + this.firstName);
+// };
+
+//jessica.greet();
+
+const walter = new PersonCl('Walter White', 1965);
+
+console.log(walter.fullName);
+walter.fullName = 'Heisenberg ';
+
+console.log(walter.fullName);
+console.log(walter.calcAge());
+
+/**
+ *  1. Classes are not hoisted unlike function constructors
+ *  2. Classes are first-class citizens
+ *  3. Classes are executed in strict mode
+ *  4. getters and setters are not functions but are used as properties.
+ *  5. {@link PersonCl | Example}
+ */
+
+const account = {
+  owner: 'Jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+// console.log(account.latest);
+// account.latest = 119;
+// console.log(account.latest);
